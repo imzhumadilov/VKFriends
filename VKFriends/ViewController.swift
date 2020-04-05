421a5097b89be54e5d779e09d9465eeca487daea
Ilyas <zhumadillov@gmail.com>
2020-04-05 16:04:46 +0300

    Initial Commit

---
+++ VKFriends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKFriends
+//
+//  Created by Ilyas Zhumadilov on 05.04.2020.
+//  Copyright © 2020 Ilyas Zhumadilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
